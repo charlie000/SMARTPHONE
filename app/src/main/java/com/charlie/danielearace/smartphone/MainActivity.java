c8f01e8898562badc8dbab1326aabe61adaaf715
dibla10 <diblasio.davide@libero.it>
2015-07-14 19:05:38 +0200

    Initial commit Costanza

--- app/src/main/java/com/charlie/danielearace/smartphone/MainActivity.java
+++ app/src/main/java/com/charlie/danielearace/smartphone/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         //jhjjlkmjmjj
         //Davide è coglione
+        //Fammi una pompa tu e Costanza
     }
 
     @Override
